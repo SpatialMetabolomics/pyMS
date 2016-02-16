2bf47fcc5e6c69252a9883fa210c6b75704be5fa
Dominik Fay <dominik.fay@gmx.de>
2016-02-16 11:45:05 +0100

    Prepare for pakaging
    - arrange files in top-level package
    - Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='pyMS',
+      version='0.1',
+      description='Python library for processing individual mass spectra',
+      url='https://github.com/alexandrovteam/pyMS',
+      author='Alexandrov Team, EMBL',
+      packages=find_packages())
