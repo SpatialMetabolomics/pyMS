da62b6f05d0d0c0361a982893f1a8a4bd25bd404
Dominik Fay <dominik.fay@gmx.de>
2016-02-18 17:45:52 +0100

    Minor metadata refurbishments
    - Remove redundant search page
    - single-source version number
    - less repetition in conf.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
+from pyMS import __version__
+
 setup(name='pyMS',
-      version='0.1',
+      version=__version__,
       description='Python library for processing individual mass spectra',
       url='https://github.com/alexandrovteam/pyMS',
       author='Alexandrov Team, EMBL',
