02e501a930a2e440b703c31c7fc011427694e2cb
andy-d-palmer <palmer@embl.de>
2016-06-16 11:36:55 +0200

    added norm "tic_range"

--- pyMSpec/normalisation.py
+++ pyMSpec/normalisation.py
@@ -2,7 +2,7 @@ __author__ = 'palmer'
 import numpy as np
 
 
-def apply_normalisation(counts, type_str=""):
+def apply_normalisation(mzs, counts, type_str="", norm_args={}):
     """
     helper function to apply a normalisation function (with some input testing etc)
     :param counts: numpy array of values to normalise
@@ -13,11 +13,13 @@ def apply_normalisation(counts, type_str=""):
                    "tic": tic,
                    "rms": rms,
                    "mad": mad,
-                   "sqrt": sqrt}
+                   "sqrt": sqrt,
+                   "tic_range": tic_range}
     if type_str not in normToApply.keys():
         raise ValueError("{} not in {}".format(type_str, normToApply.keys()))
+    mzs = np.asarray(mzs, dtype=float)
     counts = np.asarray(counts, dtype=float)
-    return normToApply[type_str](counts)
+    return normToApply[type_str](mzs, counts, **norm_args)
 
 
 def shift_and_scale(counts, scale=1.0, shift=0.0):
@@ -48,7 +50,7 @@ def check_zeros(counts):
         return False
 
 
-def none(counts):
+def none(mzs,counts):
     """
     does nothing, just returns input. is a dummy for programmatic case where a function must be supplied
     :param counts:  numpy array
@@ -57,7 +59,7 @@ def none(counts):
     return np.asarray(counts, dtype=float)
 
 
-def tic(counts):
+def tic(mzs, counts):
     """
     normalisation function, divides each intensity by the sum of all intensities (each spectrum sums to 1)
     :param counts: numpy array
@@ -67,8 +69,18 @@ def tic(counts):
         return counts
     return shift_and_scale(counts, scale=np.sum(counts))
 
+def tic_range(mzs, counts, range):
+    """
+    normalisation function, divides each intensity by the sum of all intensities (each spectrum sums to 1)
+    :param counts: numpy array
+    :return:counts normalised: numpy array
+    """
+    if check_zeros(counts):
+        return counts
+    vector_mask = np.all([mzs>range[0], mzs<range[1]], axis=0)
+    return shift_and_scale(counts, scale=np.sum(counts[vector_mask]))
 
-def rms(counts):
+def rms(mzs, counts):
     """
     normalisation function, divides each intensity by the root-mean-square of all intensities
     :param counts: numpy array
@@ -79,7 +91,7 @@ def rms(counts):
     return shift_and_scale(counts, scale=np.sqrt(np.mean(np.square(counts, dtype=float))))
 
 
-def mad(counts):
+def mad(mzs, counts):
     """
     normalisation function, divides each intensity by the median-absolute-deviation of all intensities
     :param counts: numpy array
@@ -90,7 +102,7 @@ def mad(counts):
     return shift_and_scale(counts, scale=np.median(np.abs(counts - np.median(counts))))
 
 
-def sqrt(counts):
+def sqrt(mzs, counts):
     """
     normalisation function, returns the square root of intensities
     :param counts: numpy array
