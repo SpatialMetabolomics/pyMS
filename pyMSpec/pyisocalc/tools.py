8723a4411f71c5e88ff8dc6efd9ce7a67caf0bc5
Artem Tarasov <artem.tarasov@embl.de>
2017-01-25 09:30:55 +0100

    tools.py: pep8, print function

--- pyMSpec/pyisocalc/tools.py
+++ pyMSpec/pyisocalc/tools.py
@@ -1,58 +1,60 @@
-def make_sf_adduct_database(sum_formulae,adducts,output_filename,sigma=0.001,resolution=10000,charge=1):
+from __future__ import print_function
+
+def make_sf_adduct_database(sum_formulae, adducts, output_filename, sigma=0.001, resolution=10000, charge=1):
     from pyMSpec.pyisocalc import pyisocalc
     # Extract variables from config dict
     # Check if already genrated and load if possible, otherwise calculate fresh
-    with open(output_filename,'a') as f_out:
+    with open(output_filename, 'a') as f_out:
         for sum_formula in sum_formulae:
-            #print sum_formula
+            # print sum_formula
             for adduct in adducts:
                 try:
-                    sf = pyisocalc.complex_to_simple(sum_formula+adduct)
-                    if sf is None: # not possible to form adduct
+                    sf = pyisocalc.complex_to_simple(sum_formula + adduct)
+                    if sf is None:  # not possible to form adduct
                         continue
 
                     isotope_ms = pyisocalc.isodist(sf, plot=False, sigma=sigma, charges=charge,
-                                               resolution=resolution)
+                                                   resolution=resolution)
                 except KeyError as e:
                     if str(e).startswith("KeyError:"):
-                        print str(e)
+                        print(str(e))
                         continue
                 except ValueError as e:
                     if str(e).startswith("Element not recognised"):
-                        print str(e)
+                        print(str(e))
                         continue
                 except:
-                    print sf=="", sum_formula, adduct
+                    print(sf == "", sum_formula, adduct)
                     raise
-                f_out.write("{},[M{}],{},{}\n".format(sum_formula,adduct,isotope_ms.get_spectrum(source='centroids')[0],isotope_ms.get_spectrum(source='centroids')[1]))
+                f_out.write("{},[M{}],{},{}\n".format(sum_formula, adduct, isotope_ms.get_spectrum(
+                    source='centroids')[0], isotope_ms.get_spectrum(source='centroids')[1]))
 
 
-def make_sf_adduct_optimusfilter(sum_formulae,adducts,output_filename,sigma=0.001,resolution=10000,charge=1):
+def make_sf_adduct_optimusfilter(sum_formulae, adducts, output_filename, sigma=0.001, resolution=10000, charge=1):
     from pyMSpec.pyisocalc import pyisocalc
     # Extract variables from config dict
     # Check if already genrated and load if possible, otherwise calculate fresh
-    with open(output_filename,'a') as f_out:
+    with open(output_filename, 'a') as f_out:
         for sum_formula in sum_formulae:
-            #print sum_formula
+            # print sum_formula
             for adduct in adducts:
                 try:
-                    sf = pyisocalc.complex_to_simple(sum_formula+adduct)
-                    if sf is None: # not possible to form adduct
+                    sf = pyisocalc.complex_to_simple(sum_formula + adduct)
+                    if sf is None:  # not possible to form adduct
                         continue
 
                     isotope_ms = pyisocalc.isodist(sf, plot=False, sigma=sigma, charges=charge,
-                                               resolution=resolution)
+                                                   resolution=resolution)
                 except KeyError as e:
                     if str(e).startswith("KeyError:"):
-                        print str(e)
+                        print(str(e))
                         continue
                 except ValueError as e:
                     if str(e).startswith("Element not recognised"):
-                        print str(e)
+                        print(str(e))
                         continue
                 except:
-                    print sf=="", sum_formula, adduct
+                    print(sf == "", sum_formula, adduct)
                     raise
-                f_out.write("{} [M{}],-1,{}\n".format(sum_formula,adduct,isotope_ms.get_spectrum(source='centroids')[0][0]))
-
-
+                f_out.write("{} [M{}],-1,{}\n".format(sum_formula, adduct,
+                                                      isotope_ms.get_spectrum(source='centroids')[0][0]))
